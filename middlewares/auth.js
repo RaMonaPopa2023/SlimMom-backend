20f8a9d0b1b27aa8eaaded08eee09a7efc17add9
RaMonaPopa2023 <128323291+RaMonaPopa2023@users.noreply.github.com>
2024-04-21 20:48:18 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,31 @@
 const { User } = require("../models");
 const jwt = require("jsonwebtoken");
 
-const { JWT_SECRET } = process.env;
+const jwtSecret = process.env.JWT_SECRET;
 
 const auth = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    throw new Error("Not authorized");
+  try {
+    const { authorization = "" } = req.headers;
+    const [bearer, token] = authorization.split(" ");
+
+    if (bearer !== "Bearer" || !token) {
+      return next(RequestError(401, "Not authorized"));
+    }
+
+    try {
+      const { id } = jwt.verify(token, jwtSecret);
+      const user = await User.findById(id);
+      if (!user || !user.token || user.token !== token) {
+        return next(RequestError(401, "Not authorized"));
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      return next(RequestError(401, "Not authorized"));
+    }
+  } catch (error) {
+    return next(error);
   }
-  const { userId } = jwt.verify(token, JWT_SECRET);
-
-  const user = await User.findById(userId);
-
-  if (!user || !user.tokens) {
-    throw new Error("Not authorized");
-  }
-
-  req.user = user;
-
-  next();
 };
 
 module.exports = auth;
